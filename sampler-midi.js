c8a9967156579600c318892597a1d5df06139870
Mark A. Matienzo <mark@matienzo.org>
2019-01-08 23:23:08 -0800

    Sampler changes
    * Use velocity mapping to determine sample speed
    * Add stereo panner
    * Whitespace cleanup ^_^

--- sampler-midi.js
+++ sampler-midi.js
@@ -1,28 +1,27 @@
 __.midi_init(function(){
-
+	
     __.midi_noteon(function(data){
-        console.log(data);
-      	__("sampler").speed(data[2]/60);
+      	console.log(data);
+      	__("sampler").speed(data[2]/63.5);
       	__("sampler").start(data[1]*0.10);
     	__().stop().play();
       	
     });
 
     __.midi_noteoff(function(data){
-      //console.log(data);
       __().stop();
 
     });
 
     __.midi_control(function(data){
-      //console.log(data);
       switch(data[1]){
         case 0:
 
         case 1:
+          __("sampler").start(data[2]*Math.random()*3.6);
+          __("sampler").end(data[2]*Math.random()*4.7);
+          __("panner").pan(((data[2]-64)/127*Math.random()).toFixed(4));
           __().stop().play();
-          __("sampler").start(data[1]*Math.random()*3.6);
-          __("sampler").end(data[1]*Math.random()*4.7);
         case 2:
       }
       
@@ -31,10 +30,7 @@ __.midi_init(function(){
 });
 
 __().
-    sampler({
-  		path:"samples/7_11_09-4_36PM.mp3",
-  		loop:"true"
-	}).
-    dac();
-
-__("sampler").start();
+  sampler({
+    path:"samples/7_11_09-4_36PM.mp3",
+    loop:"true"
+  }).panner().dac();
